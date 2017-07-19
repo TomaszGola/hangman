411c48a6c80441eb5eb49af2ee59bce0c2f315b7
Tomasz Gola <gola92@gmail.com>
2017-07-19 18:20:38 +0200

    add function split

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ class App extends Component {
     var start = document.getElementById("start")
     const word_API = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
 
-    console.log(start)
-
     function fetchWord(){
       fetch(word_API).then(
         resp => {
           if (resp.status === 200){
             resp.json().then(
               word =>
-              console.log(word[0].word)
+              wordSplit(word[0].word)
             )
           }
           else {
@@ -26,7 +24,13 @@ class App extends Component {
       )
     }
 
+    function wordSplit(x){
+      console.log(x);
+      console.log(x.split(''))
+    }
+
     start.addEventListener("click", fetchWord)
+
 
   }
   render() {
