3fa0b7d3e92b553ff9ff763ba9440cd1198cfbe7
Tomasz Gola <gola92@gmail.com>
2017-07-19 22:12:45 +0200

    little css changes, add svg images

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
 
     return (
 
-        <div className="App-header">
+        <div className="mainDiv">
           <h2>Wisielec</h2>
           <button id="start">start</button>
           <br/>
