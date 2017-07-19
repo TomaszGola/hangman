95885d57f63524e19807daa106c89694e6b4d050
Tomasz Gola <gola92@gmail.com>
2017-07-19 17:37:49 +0200

    fetching random word from wordnik api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+
+    var start = document.getElementById("start")
+    const word_API = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+
+    console.log(start)
+
+    function fetchWord(){
+      fetch(word_API).then(
+        resp => {
+          if (resp.status === 200){
+            resp.json().then(
+              word =>
+              console.log(word[0].word)
+            )
+          }
+          else {
+            console.log('błąd :(')
+          }
+        }
+      )
+    }
+
+    start.addEventListener("click", fetchWord)
+
+  }
   render() {
+
     return (
-      <div className="App">
+
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Wisielec</h2>
+          <button id="start">start</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
     );
   }
 }
