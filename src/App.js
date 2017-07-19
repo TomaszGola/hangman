e01e3496d922d1f736df2819b2e1fa35e5273208
Tomasz Gola <gola92@gmail.com>
2017-07-19 20:25:42 +0200

    add positions for letters

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 class App extends Component {
   componentDidMount(){
 
-    var start = document.getElementById("start")
+    var start = document.getElementById("start");
+    var placeForWord = document.getElementById("placeForWord");
     const word_API = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
     var randomWord
 
@@ -29,22 +30,31 @@ class App extends Component {
       console.log(x);
       // console.log(x.split(''));
       randomWord = x.split('')
+      showNullLetters()
     }
 
+    function showNullLetters() {
+      var nullLetter = '<span> _ </span>';
+      for(var x=0; x<randomWord.length; x++){
+        placeForWord.insertAdjacentHTML('afterbegin', nullLetter)
+      }
+    }
     // pomyśleć o klawiszach typu shift i enter...
     function letterEvent(e){
       console.log(randomWord);
       console.log(e.key);
       for(var i=0; i<randomWord.length; i++){
         if (randomWord[i] === e.key){
-          console.log('wow')
+          console.log('trafione')
         }
         else {
-          console.log('dupa')
+          console.log('nietrafione')
          }
       }
-
     }
+
+
+
 
     start.addEventListener("click", fetchWord);
     document.addEventListener('keydown', letterEvent)
@@ -57,6 +67,9 @@ class App extends Component {
         <div className="App-header">
           <h2>Wisielec</h2>
           <button id="start">start</button>
+          <div id='placeForWord'>
+
+          </div>
         </div>
 
     );
