3e9566ce82e07c6f15ceae96f2d4391e3ed016b4
Tomasz Gola <gola92@gmail.com>
2017-07-21 21:51:16 +0200

    add used letters to html

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
     var start = document.getElementById("start");
     var placeForWord = document.getElementById("placeForWord");
+    var placeForTypedLetters = document.getElementById("placeForTypedLetters");
     const word_API = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
     var randomWord
     var randomWordWithoutDuplication
@@ -31,11 +32,8 @@ class App extends Component {
     }
 
     function wordSplit(x){
-      console.log(x);
-      // console.log(x.split(''));
       randomWord = x.toLowerCase().split('')
       randomWordWithoutDuplication = [...new Set(randomWord)];
-      console.log(randomWordWithoutDuplication);
       showNullLetters()
     }
 
@@ -46,18 +44,12 @@ class App extends Component {
       }
     }
 
-    // pomyśleć o klawiszach typu shift i enter...
-    function letterEvent(e){
-      console.log(randomWord);
-      // console.log(randomWord.length);
-      // console.log(e.key);
 
+    function letterEvent(e){
       if(randomWord.indexOf(e.key) !== -1 ){
         lettersCorrect.push(e.key)
-        // console.log('trafione ' + e.key)
       } else {
         lettersMissed.push(e.key)
-        // console.error('nie trafione ' + e.key)
       }
 
       if(lettersCorrect.length === randomWordWithoutDuplication.length){
@@ -72,11 +64,10 @@ class App extends Component {
         if (randomWord[i] === e.key){
           document.getElementById(i).innerHTML = ' ' + e.key + ' ';
           }
-        }
       }
 
-
-
+      placeForTypedLetters.insertAdjacentHTML('beforebegin', e.key);  
+    }
 
     start.addEventListener("click", fetchWord);
     document.addEventListener('keydown', letterEvent)
@@ -91,6 +82,10 @@ class App extends Component {
           <button id="start">start</button>
           <br/>
           <div id='placeForWord'>
+
+          </div>
+          <br/>
+          <div id='placeForTypedLetters'>
 
           </div>
         </div>
