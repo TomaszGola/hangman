a77e41b6df6825f8fdb59769c05ca0d629693035
Tomasz Gola <gola92@gmail.com>
2017-07-21 19:40:06 +0200

    hecking if letter is correct or not

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ class App extends Component {
     const word_API = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
     var randomWord
     var i
-    var numberOfLife = 7
-    var numberOfGuessedLetters = 0
-    var arrayWithGuessedLetters = []
-    var arrayWithAllTypedLetters = []
 
     function fetchWord(){
       fetch(word_API).then(
@@ -48,33 +44,24 @@ class App extends Component {
     // pomyśleć o klawiszach typu shift i enter...
     function letterEvent(e){
       console.log(randomWord);
-      console.log(randomWord.length);
-      console.log(e.key);
-      arrayWithAllTypedLetters.push(e.key);
+      // console.log(randomWord.length);
+      // console.log(e.key);
+
+      if(randomWord.indexOf(e.key) !== -1 ){
+        console.log('trafione ' + e.key)
+      } else {
+        console.error('nie trafione ' + e.key)
+      }
+
+
+
       for(i=0; i<randomWord.length; i++){
         if (randomWord[i] === e.key){
-
-
           document.getElementById(i).innerHTML = ' ' + e.key + ' ';
-          numberOfGuessedLetters += 1;
-          arrayWithGuessedLetters.push(e.key);
-
-          // console.error(numberOfGuessedLetters);
-          if(numberOfGuessedLetters === randomWord.length){
-            // console.error('wygrałeś!!')
           }
         }
-        else {
-          console.log('nietrafione')
-         }
       }
-          console.error('arrayWithGuessedLetters');
-          console.log(arrayWithGuessedLetters);
-          console.error('arrayWithAllTypedLetters');
-          console.log(arrayWithAllTypedLetters)
 
-      // if(){}
-    }
 
 
 
